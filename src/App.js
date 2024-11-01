c8488ce9026c2552174e62b626a433b65a44773a
MinaNagy99 <minanagykhalefa@gmail.com>
2024-11-01 14:48:03 +0200

    d

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage.jsx"; // Import HomePage
 import AboutPage from "./pages/About/AboutPage.jsx"; // Import AboutPage
 import Industries from "./pages/Industries/Industries.jsx";
-import WhoWeAre from "./pages/WhoWeAre/WhoWeAre.jsx";
 
 function App() {
   return (
     <>
       <Layout>
         <Routes>
-          <Route path="/" element={<WhoWeAre />} />
+          <Route path="/" element={<Industries />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Layout>
